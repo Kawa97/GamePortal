31e53bb6b2aa3d56ad23982df380e7ce7c735921
giacomo-mangugan <giacomomangugan09091998@gmail.com>
2018-03-21 11:21:15 +0100

    detailGame creato

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,15 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './component/home/home.component';
 import { ListComponent } from './component/list/list.component';
+import { DetailGameComponent } from './component/detail-game/detail-game.component';
 
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
-    ListComponent
+    ListComponent,
+    DetailGameComponent
   ],
   imports: [
     BrowserModule
